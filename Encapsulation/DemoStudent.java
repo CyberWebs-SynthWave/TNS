d864b8c2a062e37b859b23aee9e42a27be6ec761
CyberWebs-SynthWave <meenakshe3@outlook.com>
2025-04-23 11:43:19 +0530

    DemoStudent.java

--- Encapsulation/DemoStudent.java
+++ Encapsulation/DemoStudent.java
@@ -1,1 +1,38 @@
+import java.util.*;
+public class DemoStudent {
+    
+    private int sid;
+    private String sname;
+     
+    public int getSid() {
+        return sid;
+    }
 
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void display() {
+        System.out.println("The student details are: " + sid + " " + sname);
+    }
+
+    public static void main(String[] args) {
+       
+        DemoStudent obj = new DemoStudent();
+        obj.setSid(66);
+        obj.setSname("Meenu");
+        System.out.println(obj.getSid());
+        System.out.println(obj.getSname());
+
+        // Optionally, use display method
+        obj.display();
+    }
+}
